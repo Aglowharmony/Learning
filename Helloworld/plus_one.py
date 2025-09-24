f0d367ef27d5f92c2b83b2b854f35c69a3a6fae6
Smit Shah <smits4920@gmail.com>
2025-09-24 23:57:48 +0530

    Add new files

--- Helloworld/plus_one.py
+++
@@ -1,9 +0,0 @@
-def plus_one(digits):
-    """Increment integer represented by array."""
-    n = len(digits)
-    for i in range(n-1, -1, -1):
-        if digits[i] < 9:
-            digits[i] += 1
-            return digits
-        digits[i] = 0
-    return [1] + digits
