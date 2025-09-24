846f29ffdf88e7338602381a29f5cb87fc3582b7
Smit Shah <smits4920@gmail.com>
2025-09-24 23:41:12 +0530

    Create convert.py

---
+++ Helloworld/Leetcodeproblem/convert.py
@@ -0,0 +1,14 @@
+def convert(s, numRows):
+    """Convert string to zigzag pattern."""
+    if numRows == 1 or numRows >= len(s):
+        return s
+    rows = [''] * numRows
+    idx, step = 0, 1
+    for char in s:
+        rows[idx] += char
+        if idx == 0:
+            step = 1
+        elif idx == numRows - 1:
+            step = -1
+        idx += step
+    return ''.join(rows)
