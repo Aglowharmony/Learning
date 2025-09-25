915dc13facdd09696332ca0217424812039d1f87
Smit Shah <smits4920@gmail.com>
2025-09-25 22:59:59 +0530

    Add new files

---
+++ Helloworld/Leetcodeproblem/three_sum_closest.py
@@ -0,0 +1,17 @@
+def three_sum_closest(nums, target):
+    """Find three integers whose sum is closest to target."""
+    nums.sort()
+    closest = float('inf')
+    for i in range(len(nums)-2):
+        l, r = i+1, len(nums)-1
+        while l < r:
+            s = nums[i] + nums[l] + nums[r]
+            if abs(s - target) < abs(closest - target):
+                closest = s
+            if s < target:
+                l += 1
+            elif s > target:
+                r -= 1
+            else:
+                return target
+    return closest
