40a53669831d0e92f4121735c143ee5daf9e3ea2
Smit Shah <smits4920@gmail.com>
2025-10-10 23:42:31 +0530

    10-10-25

---
+++ Helloworld/New folder/water.py
@@ -0,0 +1,14 @@
+from soil import sample
+
+
+def main():
+    moisture = sample()
+    print(f"Moisture is {moisture}%")
+         
+    while moisture > 20:
+        moisture = sample()
+        print(f"Moisture is {moisture}%")
+       
+    print("time to water")                      
+
+main()
