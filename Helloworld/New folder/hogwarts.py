7efc4343500b2c7cff46890828fa6d38af04f755
Smit Shah <smits4920@gmail.com>
2025-10-12 23:26:40 +0530

    12-10-25

--- Helloworld/New folder/hogwarts.py
+++
@@ -1,5 +0,0 @@
-students = ["Hermione", "Harry", "Ron", "drago"]
-houses = ["gryffindoor", "habibidoor", "door", "gry"]
-
-for i in range (len(students)):
-    print (i + 1, students[i])
--- Helloworld/New folder/read_json.py
+++
@@ -1,9 +0,0 @@
-# let's read and change some exisiting information 
-
-import json 
-
-with open('game_save.json', 'r') as f:
-    json.load(f)
-
-print(users)
-new_user_name = input
