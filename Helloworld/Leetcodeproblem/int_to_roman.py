4f054855030633dc373e0857c69fac44e6380758
Smit Shah <smits4920@gmail.com>
2025-10-14 23:25:44 +0530

    14-10-25

---
+++ Helloworld/Leetcodeproblem/int_to_roman.py
@@ -0,0 +1,10 @@
+def int_to_roman(num):
+    """Convert integer to Roman numeral."""
+    val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    syb = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    res = ''
+    for i in range(len(val)):
+        while num >= val[i]:
+            num -= val[i]
+            res += syb[i]
+    return res
