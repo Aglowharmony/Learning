80e93cb06e6157fdd5115f09a10f25a7096a07e9
Smit Shah <smits4920@gmail.com>
2025-10-18 23:48:37 +0530

    18-10-25

---
+++ Helloworld/Leetcodeproblem/search_insert.py
@@ -0,0 +1,10 @@
+def search_insert(nums, target):
+    """Find index to insert target."""
+    l, r = 0, len(nums)
+    while l < r:
+        m = (l + r) // 2
+        if nums[m] < target:
+            l = m + 1
+        else:
+            r = m
+    return l
