f9df5cbbf440db971a11d12ac7bb83f136a33b65
Smit Shah <smits4920@gmail.com>
2025-11-10 23:48:13 +0530

    10-11-25

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,4 @@
-import * as fs from './fs';
-export { fs };
+/// <reference types="node" />
+import type * as fs from 'fs';
+export declare type Stats = fs.Stats;
+export declare type ErrnoException = NodeJS.ErrnoException;
