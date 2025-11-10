93d5d8d67c2ae360ff96f592d4626d482a3c79af
Smit Shah <smits4920@gmail.com>
2025-11-10 23:47:59 +0530

    10-11-25

--- sync.d.ts
+++ sync.d.ts
@@ -1,5 +1,3 @@
 import type Settings from '../settings';
-import type { Entry } from '../types';
-export declare function read(directory: string, settings: Settings): Entry[];
-export declare function readdirWithFileTypes(directory: string, settings: Settings): Entry[];
-export declare function readdir(directory: string, settings: Settings): Entry[];
+import type { Stats } from '../types';
+export declare function read(path: string, settings: Settings): Stats;
