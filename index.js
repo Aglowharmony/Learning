d80e0c4c4d9aca2effbf2cf96fed8ce264b764fc
Smit Shah <smits4920@gmail.com>
2025-11-16 22:35:39 +0530

    16-11-25

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 "use strict";
 
 module.exports = {
-    array: require("./array"),
-    function: require("./function"),
-    map: require("./map"),
-    object: require("./object"),
-    set: require("./set"),
-    string: require("./string"),
+    global: require("./global"),
+    calledInOrder: require("./called-in-order"),
+    className: require("./class-name"),
+    deprecated: require("./deprecated"),
+    every: require("./every"),
+    functionName: require("./function-name"),
+    orderByFirstCall: require("./order-by-first-call"),
+    prototypes: require("./prototypes"),
+    typeOf: require("./type-of"),
+    valueToString: require("./value-to-string"),
 };
