27d613001ab3b3746d4b6de591dd58113604472a
Smit Shah <smits4920@gmail.com>
2025-11-18 23:33:01 +0530

    18-11-25

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-"use strict";
-
-module.exports = {
-    global: require("./global"),
-    calledInOrder: require("./called-in-order"),
-    className: require("./class-name"),
-    deprecated: require("./deprecated"),
-    every: require("./every"),
-    functionName: require("./function-name"),
-    orderByFirstCall: require("./order-by-first-call"),
-    prototypes: require("./prototypes"),
-    typeOf: require("./type-of"),
-    valueToString: require("./value-to-string"),
-};
+import _default from '../html';
+export { _default as default };
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9jb2RlQmxvY2svaW5kZXguanMiXSwibmFtZXMiOlsiZGVmYXVsdCJdLCJtYXBwaW5ncyI6InFCQUFvQixTO3FCQUFiQSxPIiwiZmlsZSI6ImluZGV4LmpzIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQgZnJvbSAnLi4vaHRtbCc7XG4iXX0=
