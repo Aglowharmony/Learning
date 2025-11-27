32474806f6965e317fc485d947d2312d038a8470
Smit Shah <smits4920@gmail.com>
2025-11-27 23:35:19 +0530

    27-11-25

--- implementation.js
+++ implementation.js
@@ -1,21 +1,37 @@
 'use strict';
 
-var callBind = require('call-bind');
-var test = require('tape');
-var hasStrictMode = require('has-strict-mode')();
+var ArrayCreate = require('es-abstract/2024/ArrayCreate');
+var CompareArrayElements = require('es-abstract/2024/CompareArrayElements');
+var CreateDataPropertyOrThrow = require('es-abstract/2024/CreateDataPropertyOrThrow');
+var IsCallable = require('es-abstract/2024/IsCallable');
+var LengthOfArrayLike = require('es-abstract/2024/LengthOfArrayLike');
+var SortIndexedProperties = require('es-abstract/2024/SortIndexedProperties');
+var ToObject = require('es-abstract/2024/ToObject');
+var ToString = require('es-abstract/2024/ToString');
 
-var implementation = require('../implementation');
-var runTests = require('./tests');
+var $TypeError = require('es-errors/type');
 
-test('as a function', function (t) {
-	t.test('bad array/this value', { skip: !hasStrictMode }, function (st) {
-		/* eslint no-useless-call: 0 */
-		st['throws'](function () { implementation.call(undefined); }, TypeError, 'undefined is not an object');
-		st['throws'](function () { implementation.call(null); }, TypeError, 'null is not an object');
-		st.end();
+module.exports = function toSorted(comparefn) {
+	if (typeof comparefn !== 'undefined' && !IsCallable(comparefn)) {
+		throw new $TypeError('`comparefn` must be a function'); // step 1
+	}
+
+	var O = ToObject(this); // step 2
+	var len = LengthOfArrayLike(O); // step 3
+	var A = ArrayCreate(len); // step 4
+
+	// eslint-disable-next-line no-sequences
+	var SortCompare = (0, function (x, y) { // step 5
+		return CompareArrayElements(x, y, comparefn); // step 5.a
 	});
 
-	runTests(callBind(implementation), t);
+	var sortedList = SortIndexedProperties(O, len, SortCompare, 'read-through-holes'); // step 6
 
-	t.end();
-});
+	var j = 0; // step 7
+	while (j < len) { // step 8
+		CreateDataPropertyOrThrow(A, ToString(j), sortedList[j]); // step 8.a
+		j += 1; // step 8.b
+	}
+
+	return A; // step 9
+};
