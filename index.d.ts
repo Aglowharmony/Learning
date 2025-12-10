6502f2b077ec428dc79a9870894b1cbc46b5a06d
Smit Shah <smits4920@gmail.com>
2025-12-10 23:51:11 +0530

    10-12-25

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,11 @@
-import type { PluginCreator } from 'postcss';
-declare type pluginOptions = {
-    preserve?: boolean;
-    enableProgressiveCustomProperties?: boolean;
-};
-declare const postcssPlugin: PluginCreator<pluginOptions>;
-export default postcssPlugin;
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+export { default as BufferedConsole } from './BufferedConsole';
+export { default as CustomConsole } from './CustomConsole';
+export { default as NullConsole } from './NullConsole';
+export { default as getConsoleOutput } from './getConsoleOutput';
+export type { ConsoleBuffer, LogMessage, LogType, LogEntry } from './types';
