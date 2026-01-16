c6559e1175e49004f30a0c7d529e892112065cba
Smit Shah <smits4920@gmail.com>
2026-01-16 23:13:45 +0530

    16-01-26

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,1 @@
-export { emitForest, emitForestLines, parseFunctionRanges, parseOffsets } from "./ascii";
-export { cloneFunctionCov, cloneProcessCov, cloneScriptCov, cloneRangeCov } from "./clone";
-export { compareScriptCovs, compareFunctionCovs, compareRangeCovs } from "./compare";
-export { mergeFunctionCovs, mergeProcessCovs, mergeScriptCovs } from "./merge";
-export { RangeTree } from "./range-tree";
-export { ProcessCov, ScriptCov, FunctionCov, RangeCov } from "./types";
+export default function tarjan<T>(graph: Map<T, Set<T>>): Array<Set<T>>
