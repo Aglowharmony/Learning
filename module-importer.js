39f6cf54352c1be45d6f58451ed0045c5f9dfeec
Smit Shah <smits4920@gmail.com>
2026-03-03 22:17:13 +0530

    03-03-26

--- module-importer.js
+++ module-importer.js
@@ -1,14 +1,10 @@
+import { createRequire } from 'module';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 /**
  * @fileoverview Universal module importer
  */
-
-//-----------------------------------------------------------------------------
-// Imports
-//-----------------------------------------------------------------------------
-
-import { createRequire } from "module";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
 
 //-----------------------------------------------------------------------------
 // Helpers
